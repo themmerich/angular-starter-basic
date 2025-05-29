bff39c497dde118ddc4a8e0d32bd5803ecd29b25
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2025-05-29 14:06:21 +0200

    added localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private translateService = inject(TranslateService);
+
+  constructor() {
+    this.translateService.addLangs(['en', 'de']);
+    this.translateService.setDefaultLang('de');
+    this.translateService.use('de');
+  }
 }
