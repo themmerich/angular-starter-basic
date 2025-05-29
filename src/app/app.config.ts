202099cbba6bf7823570e5eb337b77e7149102d0
Thomas Hemmerich <t.hemmerich.extern@drgueldener.de>
2025-05-29 16:35:04 +0200

    added primeng and tailwind

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,10 @@ import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { ConfirmationService } from 'primeng/api';
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
   new TranslateHttpLoader(http, './i18n/', '.json');
@@ -14,6 +18,19 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind, primeng',
+          },
+          darkModeSelector: '.dark',
+        },
+      },
+    }),
     provideTranslateService({
       defaultLanguage: 'de',
       loader: {
@@ -22,5 +39,6 @@ export const appConfig: ApplicationConfig = {
         deps: [HttpClient],
       },
     }),
+    ConfirmationService
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './core/feat-navigation/shell/shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: ShellComponent,
+    title: 'SkillFlowAI',
+  }
+];
